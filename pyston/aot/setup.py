a7bcab917cd9547955399a1a424cc7c2177139ab
Kevin Modzelewski <kevmod@gmail.com>
2021-09-29 21:54:04 -0400

    still hacky but should work for marius and as we commit to numpy

--- pyston/aot/setup.py
+++ pyston/aot/setup.py
@@ -1,3 +1,5 @@
+import glob
+import os
 def configuration(parent_package='', top_path=None):
     import numpy
     from numpy.distutils.misc_util import Configuration
@@ -5,7 +7,9 @@ def configuration(parent_package='', top_path=None):
     config = Configuration(None,#'numpy_pyston',
                            parent_package,
                            top_path)
-    config.add_extension('numpy_pyston', ['aot_numpy_init.c'], extra_objects=['../../build/aot_numpy/aot_numpy_all.o'])
+
+    umath_so = max(glob.glob( "../../build/unopt_env/lib/python3.8-pyston2.3/site-packages/numpy-*/numpy/core/_multiarray_umath.pyston-23-x86_64-linux-gnu.so"))
+    config.add_extension('numpy_pyston', ['aot_numpy_init.c'], extra_objects=['../../build/aot_numpy/aot_numpy_all.o', os.path.abspath(os.path.join(os.path.dirname(__file__), umath_so))])
 
     return config
 
