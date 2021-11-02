b4f54fc397a74fd3b260b455b8aa10b1f0c115f0
Kevin Modzelewski <kevmod@gmail.com>
2021-11-02 16:19:33 -0400

    make_order: filter out things that appear in noarch
    Previously it was building anything that appeared in linux-64,
    but there is at least one package (attrs) which started as a linux-64 package
    and is now a noarch package.
    
    This seems to affect quite a few packages, more than I would expect though.

--- pyston/conda/build_condaforge.sh
+++ pyston/conda/build_condaforge.sh
@@ -8,11 +8,11 @@ which anaconda
 
 conda search '*' -c $CHANNEL --override-channels | tail -n +3 | awk '{print $1}' > /tmp/packages.txt
 
-while read pkg; do
+python3 pyston/conda/make_order.py numpy scipy tensorflow | while read pkg; do
     if grep -q -i $pkg /tmp/packages.txt; then
         echo $pkg already built
         continue
     fi
     bash $(dirname $0)/build_feedstock.sh $pkg
     anaconda upload -u kmod --label dev $(find $pkg-feedstock/build_artifacts/ -name '*.tar.bz2' | grep -v broken)
-done < $(dirname $0)/pyston-python-order.txt
+done
--- pyston/conda/make_order.py
+++ pyston/conda/make_order.py
@@ -20,12 +20,18 @@ _feedstock_overrides = {
     "atk-1.0": "atk",
     "pybind11-abi": "pybind11",
     "pybind11-global": "pybind11",
+    "poppler-qt": "poppler",
+    "cross-r-base": "r-base",
+    "tensorflow-base": "tensorflow",
+    "tensorflow-estimator": "tensorflow",
+    "libtensorflow": "tensorflow",
+    "libtensorflow_cc": "tensorflow",
 }
 def getFeedstockName(pkg):
     return _feedstock_overrides.get(pkg, pkg)
 
 def getBuildRequirements(pkg):
-    reponame = pkg + "-feedstock"
+    reponame = getFeedstockName(pkg) + "-feedstock"
 
     if not os.path.exists(reponame):
         subprocess.check_call(["git", "clone", "https://github.com/conda-forge/" + reponame], stdin=open("/dev/null"))
@@ -34,16 +40,23 @@ def getBuildRequirements(pkg):
 
 verbose = 0
 _depends_on_python = {"python": True}
-def depends_on_python(pkg, packages):
-    pkg = getFeedstockName(pkg)
+listed = set()
+
+packages_by_name = {}
+noarch_packages = set()
+
+def depends_on_python(pkg):
     if pkg not in _depends_on_python:
         _depends_on_python[pkg] = False
-        if pkg not in packages:
+        if pkg not in packages_by_name:
+            return False
+        if pkg in noarch_packages:
+            # print(pkg, "is noarch")
             return False
         r = False
-        for d in packages[pkg]['depends']:
+        for d in packages_by_name[pkg]['depends']:
             dn = d.split()[0]
-            subdepends = depends_on_python(dn, packages)
+            subdepends = depends_on_python(dn)
             r = subdepends or r
             if subdepends and verbose:
                 print(pkg, "run depends on", d)
@@ -52,12 +65,16 @@ def depends_on_python(pkg, packages):
 
         if r and pkg not in ("python_abi", "certifi", "setuptools"):
             for b in getBuildRequirements(pkg):
-                if b in ("conda", "conda-smithy"):
+                if b in ("conda", "conda-smithy", "flaky", "nose"):
                     continue
-                subdepends = depends_on_python(b, packages)
+                subdepends = depends_on_python(b)
                 if subdepends and verbose:
                     print(pkg, "build depends on", b)
-            print(pkg)
+
+            name = getFeedstockName(pkg)
+            if name not in listed:
+                print(name)
+                listed.add(name)
     return _depends_on_python[pkg]
 
 def main(targets):
@@ -67,22 +84,37 @@ def main(targets):
         subprocess.check_call(["wget", "https://conda.anaconda.org/conda-forge/linux-64/repodata.json.bz2", "-O", repodata_fn + ".bz2"])
         subprocess.check_call(["bzip2", "-d", repodata_fn + ".bz2"])
 
+    repodata_noarch_fn = "repodata_condaforge_noarch.json"
+    if not os.path.exists(repodata_noarch_fn):
+        print("Downloading...")
+        subprocess.check_call(["wget", "https://conda.anaconda.org/conda-forge/noarch/repodata.json.bz2", "-O", repodata_noarch_fn + ".bz2"])
+        subprocess.check_call(["bzip2", "-d", repodata_noarch_fn + ".bz2"])
+
     if verbose:
         print("Loading...")
     data = json.load(open(repodata_fn))
+    data_noarch = json.load(open(repodata_noarch_fn))
     if verbose:
         print("Analyzing...")
     packages = data["packages"]
 
-    packages_by_name = {}
     for k, v in packages.items():
         packages_by_name[v['name']] = v
 
+    for k, v in data_noarch["packages"].items():
+        noarch_packages.add(v['name'])
+
     for name in targets:
-        depends_on_python(name, packages_by_name)
+        depends_on_python(name)
 
 if __name__ == "__main__":
     targets = sys.argv[1:]
+
+    if "-v" in targets:
+        verbose = True
+        targets.remove("-v")
+
     if not targets:
         targets = ["Pillow", "urllib3", "numpy", "scipy", "pandas", "tensorflow", "pytorch"]
+
     main(targets)
