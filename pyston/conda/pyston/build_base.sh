553d872048f8d96fb402c26e9ed472df46c55f31
Marius Wachtler <undingen@gmail.com>
2021-11-05 16:46:49 +0100

    fd test

--- pyston/conda/pyston/build_base.sh
+++ pyston/conda/pyston/build_base.sh
@@ -4,24 +4,38 @@ set -eux
 # pyston should not compile llvm and bolt but instead use the conda packages
 export PYSTON_USE_SYS_BINS=1
 
+rm -rf build
+
+#########################################################################################
+# Following code is mostly copied from the cpython recipe with some changes
+# https://github.com/AnacondaRecipes/python-feedstock/blob/master-3.8/recipe/build_base.sh
+
+# Remove bzip2's shared library if present,
+# as we only want to link to it statically.
+# This is important in cases where conda
+# tries to update bzip2.
+#find "${PREFIX}/lib" -name "libbz2*${SHLIB_EXT}*" | xargs rm -fv {}
+
+# Prevent lib/python${VER}/_sysconfigdata_*.py from ending up with full paths to these things
+# in _build_env because _build_env will not get found during prefix replacement, only _h_env_placeh ...
+AR=$(basename "${AR}")
+
 # the conda compiler is named 'x86_64-conda-linux-gnu-cc' but cpython compares
 # the name to *gcc*, *clang* in the configure file - so we have to use the real name.
-# Code mostly copied from the cpython recipe.
-AR=$(basename "${AR}")
 CC=$(basename "${GCC}")
 CXX=$(basename "${CXX}")
 RANLIB=$(basename "${RANLIB}")
 READELF=$(basename "${READELF}")
 
-# overwrite default conda build flags else the bolt instrumented binary will not work
-CFLAGS="-isystem ${PREFIX}/include"
-LDFLAGS="-Wl,-rpath,${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib -L${PREFIX}/lib"
 CPPFLAGS="-isystem ${PREFIX}/include"
 
-# without this line we can't find zlib and co..
-CPPFLAGS=${CPPFLAGS}" -I${PREFIX}/include"
+CFLAGS=" -pipe"
+#CFLAGS=" -march=nocona -mtune=haswell -ftree-vectorize -ffunction-sections -pipe"
+#CFLAGS=" -march=nocona -mtune=haswell -ftree-vectorize -O2 -ffunction-sections -pipe"
 
-rm -rf build
+
+#asdadas
+export CPPFLAGS CFLAGS CXXFLAGS LDFLAGS
 
 # This causes setup.py to query the sysroot directories from the compiler, something which
 # IMHO should be done by default anyway with a flag to disable it to workaround broken ones.
@@ -46,6 +60,15 @@ _common_configure_args+=(--enable-loadable-sqlite-extensions)
 _common_configure_args+=(--with-openssl="${PREFIX}")
 _common_configure_args+=(--with-tcltk-includes="-I${PREFIX}/include")
 _common_configure_args+=("--with-tcltk-libs=-L${PREFIX}/lib -ltcl8.6 -ltk8.6")
+
+#########################################################################################
+
+# overwrite default conda build flags else the bolt instrumented binary will not work
+#CFLAGS="-isystem ${PREFIX}/include"
+#LDFLAGS="-Wl,-rpath,${PREFIX}/lib -Wl,-rpath-link,${PREFIX}/lib -L${PREFIX}/lib"
+
+# without this line we can't find zlib and co..
+CPPFLAGS=${CPPFLAGS}" -I${PREFIX}/include"
 
 export CONFIGURE_EXTRA_FLAGS='${_common_configure_args[@]} --oldincludedir=${BUILD_PREFIX}/${HOST}/sysroot/usr/include'
 
@@ -77,3 +100,34 @@ mkdir -p ${PREFIX}/lib/python${PYTHON_VERSION2}/site-packages || true
 cp ${PREFIX}/lib/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}/site-packages/README.txt ${PREFIX}/lib/python${PYTHON_VERSION2}/site-packages
 rm -r ${PREFIX}/lib/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}/site-packages
 ln -s ${PREFIX}/lib/python${PYTHON_VERSION2}/site-packages/ ${PREFIX}/lib/python${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}/site-packages
+
+#########################################################################################
+# Following code is mostly copied from the cpython recipe with some changes
+# https://github.com/AnacondaRecipes/python-feedstock/blob/master-3.8/recipe/build_base.sh
+
+VER=${PYTHON_VERSION2}-pyston${PYSTON_VERSION2}
+VERABI=${VER}
+
+# Remove test data to save space
+# Though keep `support` as some things use that.
+# TODO :: Make a subpackage for this once we implement multi-level testing.
+pushd ${PREFIX}/lib/python${VER}
+  mkdir test_keep
+  mv test/__init__.py test/support test/test_support* test/test_script_helper* test_keep/
+  rm -rf test */test
+  mv test_keep test
+popd
+
+# Size reductions:
+# pushd ${PREFIX}
+#   if [[ -f lib/libpython${VERABI}.a ]]; then
+#     chmod +w lib/libpython${VERABI}.a
+#     ${STRIP} -S lib/libpython${VERABI}.a
+#   fi
+#   CONFIG_LIBPYTHON=$(find lib/python${VER}/config-${VERABI}* -name "libpython${VERABI}.a")
+#   if [[ -f lib/libpython${VERABI}.a ]] && [[ -f ${CONFIG_LIBPYTHON} ]]; then
+#     chmod +w ${CONFIG_LIBPYTHON}
+#     rm ${CONFIG_LIBPYTHON}
+#   fi
+# popd
+#########################################################################################
