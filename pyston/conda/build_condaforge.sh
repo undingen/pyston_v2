7b052abc5759798f7de1f97b4e75854c4e2d1386
Marius Wachtler <undingen@gmail.com>
2021-11-11 16:47:32 +0100

    use undingen channel

--- pyston/conda/build_condaforge.sh
+++ pyston/conda/build_condaforge.sh
@@ -6,14 +6,17 @@ export CHANNEL
 which conda
 which anaconda
 
-conda search '*' -c $CHANNEL --override-channels | tail -n +3 | awk '{print $1}' > /tmp/packages.txt
+conda search '*' -c $CHANNEL -c undingen/label/dev --override-channels | tail -n +3 | awk '{print $1}' > /tmp/packages.txt
 
 python3 pyston/conda/make_order.py $@ | while read pkg; do
+    echo "########################################"
+    echo "$pkg"
+    echo "########################################"
     if grep -q -i $pkg /tmp/packages.txt; then
         echo $pkg already built
         continue
     fi
     CI=1 bash $(dirname $0)/build_feedstock.sh $pkg
-    anaconda upload -u kmod --label dev $(find $pkg-feedstock/build_artifacts/ -name '*.tar.bz2' | grep -v broken)
+    anaconda upload -u undingen --label dev $(find $pkg-feedstock/build_artifacts/ -name '*.tar.bz2' | grep -v broken)
     rm -rfv $pkg-feedstock/build_artifacts
 done
