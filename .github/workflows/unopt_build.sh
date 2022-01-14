3c951c0751d9503e03ca6ce419faf1e87611faf6
Marius Wachtler <undingen@gmail.com>
2022-01-14 16:46:00 +0100

    non root

--- .github/workflows/unopt_build.sh
+++ .github/workflows/unopt_build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -eux
 
-export DEBIAN_FRONTEND=noninteractive
-
 # workaround for setuptools 60
 export SETUPTOOLS_USE_DISTUTILS=stdlib
 
 # install dependencies
-apt-get update
-apt-get install -y build-essential ninja-build git cmake clang llvm libssl-dev libsqlite3-dev luajit python3.8 zlib1g-dev virtualenv libjpeg-dev linux-tools-common linux-tools-generic linux-tools-`uname -r`
+sudo apt-get update
+apt-get install -y build-essential ninja-build git cmake clang llvm libssl-dev libsqlite3-dev luajit python3.8 zlib1g-dev virtualenv libjpeg-dev linux-tools-common linux-tools-generic
 apt-get install -y libwebp-dev libjpeg-dev python3.8-gdbm python3.8-tk python3.8-dev tk-dev libgdbm-dev libgdbm-compat-dev liblzma-dev libbz2-dev nginx time
 apt-get install -y llvm-dev
 
@@ -19,12 +17,10 @@ cd /pyston_dir
 make unopt -j$(nproc)
 
 # running the tests
-
 git submodule update --init "pyston/test/*"
 # have to install late because it will update llvm-10 to 13 or so
 apt-get install -y rustc
 
 make tests -j$(nproc)
-
 
 make test -j$(nproc)
