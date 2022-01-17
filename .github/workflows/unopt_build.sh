2bfd5f086c53789efd286caf96595fd90f0fcbe0
Marius Wachtler <undingen@gmail.com>
2022-01-17 10:54:16 +0100

    f

--- .github/workflows/unopt_build.sh
+++ .github/workflows/unopt_build.sh
@@ -10,7 +10,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y build-essential ninja-build git cmake clang llvm libssl-dev libsqlite3-dev luajit python3.8 zlib1g-dev virtualenv libjpeg-dev linux-tools-common linux-tools-generic
 sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y libwebp-dev libjpeg-dev python3.8-gdbm python3.8-tk python3.8-dev tk-dev libgdbm-dev libgdbm-compat-dev liblzma-dev libbz2-dev nginx time
-apt-get install -y llvm-dev
+sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y llvm-dev
 
 export PYSTON_USE_SYS_BINS=1 # build has to use system llvm and clang binaries
 
