6dd007dfa34b974c467061085a2d8fc9ef36bf75
Marius Wachtler <undingen@gmail.com>
2022-01-17 10:49:17 +0100

    preserve-env

--- .github/workflows/unopt_build.sh
+++ .github/workflows/unopt_build.sh
@@ -8,8 +8,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 # install dependencies
 sudo apt-get update
-sudo apt-get install -y build-essential ninja-build git cmake clang llvm libssl-dev libsqlite3-dev luajit python3.8 zlib1g-dev virtualenv libjpeg-dev linux-tools-common linux-tools-generic
-sudo apt-get install -y libwebp-dev libjpeg-dev python3.8-gdbm python3.8-tk python3.8-dev tk-dev libgdbm-dev libgdbm-compat-dev liblzma-dev libbz2-dev nginx time
+sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y build-essential ninja-build git cmake clang llvm libssl-dev libsqlite3-dev luajit python3.8 zlib1g-dev virtualenv libjpeg-dev linux-tools-common linux-tools-generic
+sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y libwebp-dev libjpeg-dev python3.8-gdbm python3.8-tk python3.8-dev tk-dev libgdbm-dev libgdbm-compat-dev liblzma-dev libbz2-dev nginx time
 apt-get install -y llvm-dev
 
 export PYSTON_USE_SYS_BINS=1 # build has to use system llvm and clang binaries
@@ -21,7 +21,7 @@ make unopt -j$(nproc)
 # running the tests
 git submodule update --init "pyston/test/*"
 # have to install late because it will update llvm-10 to 13 or so
-sudo apt-get install -y rustc
+sudo --preserve-env=DEBIAN_FRONTEND apt-get install -y rustc
 
 #make tests -j$(nproc)
 
